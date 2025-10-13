b962a7d7931f38938d68f1ab086378d602b0316b
Artcrafterrra <if.artcraft@gmail.com>
2025-10-14 00:02:58 +0300

    feature: added configuration for redis and celery

---
+++ src/celery_app.py
@@ -0,0 +1,14 @@
+import os
+
+from celery import Celery
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis_theater")
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+REDIS_DB = os.getenv("REDIS_DB", "0")
+
+REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+
+celery_app = Celery(
+    "movie_streaming_api",
+    broker=REDIS_URL
+)
