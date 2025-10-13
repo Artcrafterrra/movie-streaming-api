34a7e4737c89eb9a86bc59bf8c85205bbee332b7
Otstoy <otstoy.sxe@gmail.com>
2025-10-13 13:41:43 +0300

    feat: 1.1.1 add UserGroupModel, UserModel and UserProfileModel models

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -1,4 +1,21 @@
+from datetime import datetime, date
 from enum import Enum
+from typing import Optional
+
+from sqlalchemy import (
+    String,
+    Boolean,
+    DateTime,
+    ForeignKey,
+    func,
+    Integer,
+    Date,
+    Text, UniqueConstraint,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
+
+from database.validators.accounts import validate_email
+from database.models.base import Base
 
 
 class UserGroupEnum(Enum):
@@ -10,3 +27,89 @@ class UserGroupEnum(Enum):
 class GenderEnum(Enum):
     MAN = "man"
     WOMAN = "woman"
+
+
+class UserGroupModel(Base):
+    __tablename__ = "user_groups"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    name: Mapped[UserGroupEnum] = mapped_column(
+        Enum(UserGroupEnum), nullable=False, unique=True
+    )
+
+    users: Mapped[list["UserModel"]] = relationship(
+        "UserModel", back_populates="group"
+    )
+
+    def __repr__(self):
+        return f"<UserGroupModel(id={self.id}, name={self.name})>"
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    email: Mapped[str] = mapped_column(
+        String(255), unique=True, nullable=False
+    )
+    _hashed_password: Mapped[str] = mapped_column(
+        "hashed_password", String(255), nullable=False
+    )
+    is_active: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+    group_id: Mapped[int] = mapped_column(
+        ForeignKey("user_groups.id", ondelete="CASCADE"), nullable=False
+    )
+    group: Mapped[UserGroupModel] = relationship("UserGroupModel", back_populates="users")
+
+    def __repr__(self):
+        return f"<UserModel(id={self.id}, email={self.email}, is_active={self.is_active})>"
+
+    def has_group(self, group_name: UserGroupEnum) -> bool:
+        return self.group.name == group_name
+
+    @validates("email")
+    def validate_email(self, key, value) -> str:
+        return validate_email(value.lower())
+
+
+class UserProfileModel(Base):
+    __tablename__ = "user_profiles"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    first_name: Mapped[Optional[str]] = mapped_column(String(100))
+    last_name: Mapped[Optional[str]] = mapped_column(String(100))
+    avatar: Mapped[Optional[str]] = mapped_column(String(255))
+    gender: Mapped[Optional[GenderEnum]] = mapped_column(Enum(GenderEnum))
+    date_of_birth: Mapped[Optional[date]] = mapped_column(Date)
+    info: Mapped[Optional[str]] = mapped_column(Text)
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False, unique=True
+    )
+    user: Mapped[UserModel] = relationship(
+        "UserModel", back_populates="profile"
+    )
+
+    __table_args__ = (UniqueConstraint("user_id"),)
+
+    def __repr__(self):
+        return (
+            f"<UserProfileModel(id={self.id}, first_name={self.first_name}, last_name={self.last_name}, "
+            f"gender={self.gender}, date_of_birth={self.date_of_birth})>"
+        )
