c956504cfb481c69c0bb4c8ed1a71311fbfd8513
Pavlo Semenikhin <pashock1337@gmail.com>
2025-10-14 01:29:16 +0300

    feat: implemented stars, genres, directors tables with m2m relations and migration

--- src/database/models/movies.py
+++ src/database/models/movies.py
@@ -8,10 +8,57 @@ from sqlalchemy import (
     UniqueConstraint,
     Float,
     Enum,
+    Table,
+    Column,
+    ForeignKey,
 )
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database.models.base import Base
+
+
+movie_stars = Table(
+    "movie_stars",
+    Base.metadata,
+    Column(
+        "movie_id",
+        ForeignKey("movies.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    Column(
+        "star_id", ForeignKey("stars.id", ondelete="CASCADE"), primary_key=True
+    ),
+)
+
+movie_directors = Table(
+    "movie_directors",
+    Base.metadata,
+    Column(
+        "movie_id",
+        ForeignKey("movies.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    Column(
+        "director_id",
+        ForeignKey("directors.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+)
+
+movie_genres = Table(
+    "movie_genres",
+    Base.metadata,
+    Column(
+        "movie_id",
+        ForeignKey("movies.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    Column(
+        "genre_id",
+        ForeignKey("genres.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+)
 
 
 class CertificationEnum(str, enum.Enum):
@@ -48,5 +95,48 @@ class Movie(Base):
         Enum(CertificationEnum), nullable=False
     )
 
+    genres: Mapped[list["Genre"]] = relationship(
+        "Genre", secondary=movie_genres, back_populates="movies"
+    )
+    stars: Mapped[list["Star"]] = relationship(
+        "Star", secondary=movie_stars, back_populates="movies"
+    )
+    directors: Mapped[list["Director"]] = relationship(
+        "Director", secondary=movie_directors, back_populates="movies"
+    )
+
     def __repr__(self) -> str:
         return f"<Movie name={self.name!r}, year={self.year}, cert={self.certification}>"
+
+
+class Genre(Base):
+    __tablename__ = "genres"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+
+    movies: Mapped[list["Movie"]] = relationship(
+        "Movie", secondary=movie_genres, back_populates="genres"
+    )
+
+
+class Star(Base):
+    __tablename__ = "stars"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+
+    movies: Mapped[list["Movie"]] = relationship(
+        "Movie", secondary=movie_stars, back_populates="stars"
+    )
+
+
+class Director(Base):
+    __tablename__ = "directors"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+
+    movies: Mapped[list["Movie"]] = relationship(
+        "Movie", secondary=movie_directors, back_populates="directors"
+    )
