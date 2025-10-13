d020f574f917ef810e54929da6a778a3e6711616
Pavlo Semenikhin <pashock1337@gmail.com>
2025-10-13 20:30:21 +0300

    feat: manage alembic with local migrations

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from typing import Any
 
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class BaseAppSettings(BaseSettings):
@@ -51,8 +53,8 @@ class Settings(BaseAppSettings):
     POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "test_db")
 
-    SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
-    SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", os.urandom(32))
+    SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32).hex())
+    SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", os.urandom(32).hex())
     JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
 
 
