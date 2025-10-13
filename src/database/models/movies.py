b523abf677319cae676bf192d67127f51a5c1df9
Pavlo Semenikhin <pashock1337@gmail.com>
2025-10-14 00:12:52 +0300

    feat: implemented movie model and certification ENUM with migration

--- src/database/models/movies.py
+++ src/database/models/movies.py
@@ -1,16 +1,52 @@
-from sqlalchemy import String, Integer, Date, Text
+import enum
+import uuid as uuid_pkg
+from sqlalchemy import (
+    String,
+    Integer,
+    Text,
+    DECIMAL,
+    UniqueConstraint,
+    Float,
+    Enum,
+)
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 from database.models.base import Base
 
 
+class CertificationEnum(str, enum.Enum):
+    G = "G"  # General Audience
+    PG = "PG"  # Parental Guidance Suggested
+    PG13 = "PG-13"  # Parents Strongly Cautioned
+    R = "R"  # Restricted
+    NC17 = "NC-17"  # Adults Only
+
+
 class Movie(Base):
     __tablename__ = "movies"
+    __table_args__ = (
+        UniqueConstraint(
+            "name", "time", "year", name="uq_movie_name_time_year"
+        ),
+    )
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    title: Mapped[str] = mapped_column(String(255), nullable=False)
-    description: Mapped[str | None] = mapped_column(Text, nullable=True)
-    release_date: Mapped[Date | None] = mapped_column(Date, nullable=True)
-    rating: Mapped[float | None] = mapped_column(nullable=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    movie_uuid: Mapped[uuid_pkg.UUID] = mapped_column(
+        UUID(as_uuid=True), default=uuid_pkg.uuid4, unique=True
+    )
+    name: Mapped[str] = mapped_column(String(250), nullable=False)
+    year: Mapped[int] = mapped_column(nullable=False)
+    time: Mapped[int] = mapped_column(nullable=False)
+    imdb: Mapped[float] = mapped_column(Float, nullable=False)
+    votes: Mapped[int] = mapped_column(nullable=False)
+    meta_score: Mapped[float | None] = mapped_column(Float)
+    gross: Mapped[float | None] = mapped_column(Float)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    price: Mapped[float] = mapped_column(DECIMAL(10, 2), nullable=False)
+
+    certification: Mapped[CertificationEnum] = mapped_column(
+        Enum(CertificationEnum), nullable=False
+    )
 
     def __repr__(self) -> str:
-        return f"<Movie id={self.id} title={self.title!r}>"
+        return f"<Movie name={self.name!r}, year={self.year}, cert={self.certification}>"
