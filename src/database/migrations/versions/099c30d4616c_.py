e649a2c93d6c21764f3ccaf9be56b82dce71d5b6
Artcrafterrra <if.artcraft@gmail.com>
2025-10-14 18:37:07 +0300

    fix: update migration to correct typo in OrderStatusEnum ("cancelled" to "canceled")

--- src/database/migrations/versions/099c30d4616c_.py
+++ src/database/migrations/versions/099c30d4616c_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 75cbd6eda565
+Revision ID: 099c30d4616c
 Revises: 1efb48109a77
-Create Date: 2025-10-14 15:12:15.357165+00:00
+Create Date: 2025-10-14 15:35:34.640836+00:00
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '75cbd6eda565'
+revision: str = '099c30d4616c'
 down_revision: Union[str, Sequence[str], None] = '1efb48109a77'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -25,7 +25,7 @@ def upgrade() -> None:
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=False),
     sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=False),
-    sa.Column('status', sa.Enum('PENDING', 'PAID', 'CANCELLED', name='orderstatusenum'), nullable=False),
+    sa.Column('status', sa.Enum('PENDING', 'PAID', 'CANCELED', name='orderstatusenum'), nullable=False),
     sa.Column('total_amount', sa.DECIMAL(precision=10, scale=2), nullable=False),
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
