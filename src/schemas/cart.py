13dd579582f54b464aa5075d1dc12ba0591f335c
yarik <super.racing@ukr.net>
2025-10-14 22:58:32 +0300

    fixed CartRead schema config

--- src/schemas/cart.py
+++ src/schemas/cart.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import List
 
 
@@ -10,8 +10,7 @@ class CartBase(BaseModel):
 class CartRead(CartBase):
     id: int
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
 
 class CartCreate(CartRead):
