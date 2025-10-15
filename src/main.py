8d021729c2d248955b7cb58199f8bffbdafbf184
Vadym <kirilkovadim193@gmail.com>
2025-10-15 11:22:59 +0300

    4.2 Refactor main for new routes
    # Conflicts:
    #	src/main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from routes import movies, accounts
+from routes.orders import router as orders_router
 
 app = FastAPI(title="Movies Api", description="Description of project")
 
@@ -8,3 +9,4 @@ api_version_prefix = "/api/v1"
 
 app.include_router(movies.router, prefix=api_version_prefix)
 app.include_router(accounts.router, prefix=api_version_prefix)
+app.include_router(orders_router)
